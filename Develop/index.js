a8db592f715c5858ea5a775f1059002beb619517
AileenWang <ainiwang14@gmail.com>
2020-08-24 16:14:49 -0700

    made questions array

---
+++ Develop/index.js
@@ -0,0 +1,64 @@
+// array of questions for user
+const questions = [
+//title, description, toc, install, usage, license, contributing, tests, questions
+    {
+        type: "input",
+        message: "Project Title?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Project Description?",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "How do you install it?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Instructions for use?",
+        name: "Usage"
+    },
+    {
+        type: "list",
+        message: "License?",
+        name: "License",
+        choices: [
+            "Apache 2.0",
+            "GNU General Public 3.0",
+            "MIT",
+            "BSD 3",
+        ]
+    },
+    {
+        type: "input",
+        message: "Contributors?",
+        name: "Contributors",
+    },
+    {
+        type: "input",
+        message: "Tests?",
+        name: "Test"
+    },
+    {
+        type: "input",
+        message: "Contact for further questions",
+        name: "Questions"
+    }
+
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
