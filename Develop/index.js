f9ee59ef9fd04880824caac905dd5329463250b8
AileenWang <ainiwang14@gmail.com>
2020-08-24 18:50:55 -0700

    aaaaAAAA

--- Develop/index.js
+++ Develop/index.js
@@ -2,32 +2,32 @@ var inquirer = require("inquirer");
 var fs = require("fs");
 
 // array of questions for user
-const questions = [
+inquirer.prompt([
 //title, description, toc, install, usage, license, contributing, tests, questions
     {
         type: "input",
+        name: "Title",
         message: "Project Title?",
-        name: "Title"
     },
     {
         type: "input",
+        name: "Description",
         message: "Project Description?",
-        name: "Description"
     },
     {
         type: "input",
+        name: "Installation",
         message: "How do you install it?",
-        name: "Installation"
     },
     {
         type: "input",
+        name: "Usage",
         message: "Instructions for use?",
-        name: "Usage"
     },
     {
         type: "list",
-        message: "License?",
         name: "License",
+        message: "License?",
         choices: [
             "Apache 2.0",
             "GNU General Public 3.0",
@@ -37,31 +37,36 @@ const questions = [
     },
     {
         type: "input",
-        message: "Contributors?",
         name: "Contributors",
+        message: "Contributors?",
     },
     {
         type: "input",
+        name: "Test",
         message: "Tests?",
-        name: "Test"
     },
     {
         type: "input",
+        name: "Questions",
         message: "Contact for further questions",
-        name: "Questions"
     }
 
 
-];
+]).then(function(data) {
 
 // function to write README file
-function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data, null, '/t'), function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log("Success!")
+    })
 }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
