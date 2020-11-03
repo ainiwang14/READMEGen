1979212b546860bedd90fd5b03269cd272447ffc
AileenWang <ainiwang14@gmail.com>
2020-11-02 17:14:31 -0800

    edited markdown creator function

---
+++ Develop/README.md
@@ -0,0 +1,30 @@
+
+# Title
+t
+## Description
+t
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation <a name="installation"></a>
+t
+
+## Usage <a name="usage"></a>
+t
+
+## License <a name="license"></a>
+Apache 2.0
+
+## Contributors <a name="contributors"></a>
+[t](t)
+
+## Tests <a name="test"></a>
+t
+
+## Questions <a name="questions"></a>
+[t](t)
--- Develop/index.js
+++ Develop/index.js
@@ -6,27 +6,27 @@ inquirer.prompt([
 //title, description, toc, install, usage, license, contributing, tests, questions
     {
         type: "input",
-        name: "Title",
+        name: "title",
         message: "Project Title?",
     },
     {
         type: "input",
-        name: "Description",
+        name: "description",
         message: "Project Description?",
     },
     {
         type: "input",
-        name: "Installation",
+        name: "installation",
         message: "How do you install it?",
     },
     {
         type: "input",
-        name: "Usage",
+        name: "usage",
         message: "Instructions for use?",
     },
     {
         type: "list",
-        name: "License",
+        name: "license",
         message: "License?",
         choices: [
             "Apache 2.0",
@@ -37,27 +37,25 @@ inquirer.prompt([
     },
     {
         type: "input",
-        name: "Contributors",
+        name: "contributors",
         message: "Contributors?",
     },
     {
         type: "input",
-        name: "Test",
+        name: "test",
         message: "Tests?",
     },
     {
         type: "input",
-        name: "Questions",
+        name: "questions",
         message: "Contact for further questions",
     }
 
 
 ]).then(function(data) {
-    
-    var fileName = data.Title.toLowerCase().split(' ').join('') + "README.json";
 
 // function to write README file
-    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function(err) {
+    fs.writeFile("README.md", generateMarkdown(data), function(err) {
         if (err) {
             throw err;
         }
@@ -72,3 +70,37 @@ inquirer.prompt([
 
 // // function call to initialize program
 // init();
+
+function generateMarkdown(data) {
+    return `
+# Title
+${data["title"]}
+## Description
+${data["description"]}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation <a name="installation"></a>
+${data["installation"]}
+
+## Usage <a name="usage"></a>
+${data["usage"]}
+
+## License <a name="license"></a>
+${data["license"]}
+
+## Contributors <a name="contributors"></a>
+[${data["contributors"]}](${data["contributors"]})
+
+## Tests <a name="test"></a>
+${data["test"]}
+
+## Questions <a name="questions"></a>
+[${data["questions"]}](${data["questions"]})
+`;
+}  
